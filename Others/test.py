db688f96cb7e90113e41789e18cace0e0d6683ca
January10 <244663831@qq.com>
2018-01-28 00:50:43 +0800

    其他：杨辉公式

--- Others/test.py
+++ Others/test.py
@@ -1,3 +1,4 @@
+# 汉诺塔
 def hanoi(n, x, y, z):
     if n == 1:
         print(x, '-->', z)
@@ -9,3 +10,15 @@ def hanoi(n, x, y, z):
 
 n = int(input('请输入汉诺塔的层数：'))
 hanoi(n, 'x', 'y', 'z')
+
+
+# 杨辉公式
+def triangles(n):
+    lst = [1]
+    for j in range(n):
+        print(lst)
+        lst.append(0)
+        lst = [lst[i - 1] + lst[i] for i in range(len(lst))]
+
+
+triangles(3)
